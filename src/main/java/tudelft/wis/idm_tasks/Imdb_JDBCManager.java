2ed4ee8327b42dfe449524a48eefc67094464559
Ivan Nikolov <I.I.Nikolov-1@student.tudelft.nl>
2024-03-14 21:22:28 +0100

    query 3 and 4

--- src/main/java/tudelft/wis/idm_tasks/Imdb_JDBCManager.java
+++ src/main/java/tudelft/wis/idm_tasks/Imdb_JDBCManager.java
@@ -13,7 +13,7 @@ public class Imdb_JDBCManager implements JDBCTask2Interface {
     public Connection getConnection()  {
         if (connection == null) {
             try {
-                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/imdb");
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/imdb","postgres","ivan221718");
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -49,16 +49,53 @@ public class Imdb_JDBCManager implements JDBCTask2Interface {
 
     public Collection<String> getJobCategoriesFromTitles(String searchString)
     {
+        String query = "SELECT primary_title FROM titles WHERE start_year = ?";
         return null;
     }
 
     public double getAverageRuntimeOfGenre(String genre)
     {
-        String
+        String query = "SELECT AVG(t.runtime) AS avg_runtime FROM titles_genres AS tg JOIN titles AS t ON t.title_id = tg.title_id WHERE tg.genre = ? GROUP BY tg.genre";
+        PreparedStatement myStmt = null;
+        try {
+            getConnection();
+            myStmt = connection.prepareStatement(query);
+            myStmt.setString(1, genre);
+            ResultSet myRs = null;
+            myRs = myStmt.executeQuery();
+            myRs.next();
+            double avg_rating = myRs.getDouble("avg_runtime");
+            return avg_rating;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public Collection<String> getPlayedCharacters(String actorFullname)
     {
-        return null;
+        String query = "SELECT DISTINCT tpc.character_name \n" +
+                "FROM persons AS p\n" +
+                "JOIN title_person_character AS tpc ON tpc.person_id = p.person_id\n" +
+                "WHERE p.full_name = ?";
+
+        PreparedStatement myStmt = null;
+        try {
+            getConnection();
+            myStmt = connection.prepareStatement(query);
+            myStmt.setString(1, actorFullname);
+            ResultSet myRs = null;
+            myRs = myStmt.executeQuery();
+
+            Collection<String> playedCharacters = new ArrayList<String>();
+            while (myRs.next()) {
+                String character = myRs.getString("character_name");
+                playedCharacters.add(character);
+            }
+
+            return playedCharacters;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/tudelft/wis/idm_tasks/Main.java
+++ src/main/java/tudelft/wis/idm_tasks/Main.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Imdb_JDBCManager imdbJdbcManager = new Imdb_JDBCManager();
-  //      Connection connection = imdbJdbcManager.getConnection();
-
         Scanner scanner = new Scanner(System.in);
+
+        //Query 1
+        System.out.println("Query-1: Please enter a year");
         int year = scanner.nextInt();
         ArrayList<String> titles = new ArrayList<>(imdbJdbcManager.getTitlesPerYear(year));
         for(int i = 0; i < 20; i++)
@@ -18,6 +19,19 @@ public class Main {
             System.out.println(titles.get(i));
         }
 
+        //Query 3
+        System.out.println("Query-3: Please enter a genre");
+        String genre = scanner.next();
+        System.out.println(imdbJdbcManager.getAverageRuntimeOfGenre(genre));
+
+        //Query 4
+        System.out.println("Query-4: Please enter an actor's name");
+        String actor = scanner.next();
+        ArrayList<String> playedCharacters = new ArrayList<>(imdbJdbcManager.getPlayedCharacters(actor));
+        for(int i = 0; i < 20; i++)
+        {
+            System.out.println(playedCharacters.get(i));
+        }
 
     }
 }
